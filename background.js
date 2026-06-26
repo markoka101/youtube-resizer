e8523bcc60906299c417d338eb7c9d67bab709a0
markoka101 <markoka101@gmail.com>
2026-06-26 14:29:18 -0400

    handle injection on different scenarios

--- background.js
+++ background.js
@@ -1,6 +1,25 @@
+function scheduleOpenSettingsModal(tabId) {
+  setTimeout(() => {
+    chrome.tabs.sendMessage(tabId, { action: "OPEN_SETTINGS_MODAL" }).catch(e => console.error(e));
+  }, 100);
+}
+
+function injectContentScript(tabId) {
+  chrome.scripting.executeScript({
+    target: { tabId },
+    files: ['content.js']
+  }).then(() => {
+    scheduleOpenSettingsModal(tabId);
+  }).catch(execErr => console.error("Script injection failed:", execErr));
+}
+
+function openSettingsModal(tabId) {
+  chrome.tabs.sendMessage(tabId, { action: "OPEN_SETTINGS_MODAL" }).catch((err) => {
+    console.log("Orphaned tab detected or content script uninitialized. Injecting manually...", err);
+    injectContentScript(tabId);
+  });
+}
+
 chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id },
-        files: ['content.js']
-    })
-})
+  openSettingsModal(tab.id);
+});
